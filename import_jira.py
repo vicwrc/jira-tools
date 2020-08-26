120b8be6be61c7f608c74ae009357953b61c64c1
viktor.vorontsov@kstt.ua <Viktor_Vorontsov@kstt.com>
2020-08-26 15:19:31 +0300

    history retrieval

--- import_jira.py
+++ import_jira.py
@@ -6,4 +6,7 @@ jira = jira_connector.jira
 jql = 'project = PR '
 issues_list = jira.search_issues(jql)
 
+issue = jira.issue('CFD-5792', expand='changelog,transitions,history')
+
 print(issues_list)
+print('\n')
